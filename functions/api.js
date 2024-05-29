bef6c089aee97c438eb177319c96978b905f88c3
marco <marco@pop-os.intelbras.local>
2024-05-29 16:57:07 -0300

    push

--- functions/api.js
+++ functions/api.js
@@ -1,53 +1,10 @@
-const Hapi = require('@hapi/hapi');
-const { builder } = require('@netlify/functions');
+const express = require('express');
+const serverless = require('@netlify/functions');
 
-const init = async () => {
-    const server = Hapi.server({
-        port: process.env.PORT || 3000,
-        host: 'localhost'
-    });
+const app = express();
 
-    server.route({
-        method: 'GET',
-        path: '/api',
-        handler: (request, h) => {
-            return { message: 'Hello from Hapi.js!' };
-        }
-    });
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello from Express.js!' });
+});
 
-    await server.initialize();
-    return server;
-};
-
-let cachedServer;
-
-const getServer = async () => {
-    if (!cachedServer) {
-        cachedServer = await init();
-    }
-    return cachedServer;
-};
-
-const handler = async (event, context) => {
-    const hapiServer = await getServer();
-
-    const { req, res } = hapiServer.listener._start(event, context);
-
-    return new Promise((resolve, reject) => {
-        res.on('finish', () => {
-            resolve({
-                statusCode: res.statusCode,
-                headers: res.getHeaders(),
-                body: res.outputPayload.payload.toString(),
-            });
-        });
-
-        res.on('error', (err) => {
-            reject(err);
-        });
-
-        hapiServer.listener.emit('request', req, res);
-    });
-};
-
-module.exports.handler = builder(handler);
+module.exports.handler = serverless.createHandler(app);
