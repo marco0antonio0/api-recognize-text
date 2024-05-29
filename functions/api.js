1ffecf5b06940f25aafbdffa0feb22331e5689c8
marco <marco@pop-os.intelbras.local>
2024-05-29 17:10:14 -0300

    push

--- functions/api.js
+++ functions/api.js
@@ -3,11 +3,9 @@ const serverless = require('serverless-http');
 
 const app = express();
 
-app.get('/api', (req, res) => {
+// Defina suas rotas de API aqui
+app.get('/api/heollo', (req, res) => {
     res.json({ message: 'Hello from Express.js!' });
 });
-
-// Adaptar o caminho para a função Netlify
-app.use('/.netlify/functions/api', app);
 
 module.exports.handler = serverless(app);
